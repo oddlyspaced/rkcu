0b138a64888306307a58ef1fe2c7eb617ed47430
oddlyspaced <oddlyspaced1@gmail.com>
2022-12-21 03:37:27 +0530

    main: test all animation types in loop

--- main.py
+++ main.py
@@ -1,19 +1,21 @@
 import hid
+import time
 from src.config import BaseConfig
+from src.enums import Animation
 
 RK61_VID = 0x258a
 RK61_PID = 0x004a
 
-config = BaseConfig()
-report = config.report()
-
-print("Sending report to Keyboard: " + report.hex(" "))
-
-rk61 = hid.enumerate(RK61_VID, RK61_PID)
-kb_path = rk61[1].get('path')
-
-h = hid.device()
-h.open_path(kb_path)
-
-h.send_feature_report(bytes(report))
-h.close()
+for i in Animation:
+    print(i)
+    config = BaseConfig()
+    config.ANIMATION_TYPE = i
+    report = config.report()
+    print("Sending report to Keyboard: " + report.hex(" "))
+    rk61 = hid.enumerate(RK61_VID, RK61_PID)
+    kb_path = rk61[1].get('path')
+    h = hid.device()
+    h.open_path(kb_path)
+    h.send_feature_report(bytes(report))
+    h.close()
+    time.sleep(5)
