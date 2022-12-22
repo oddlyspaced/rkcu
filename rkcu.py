b2cff0ad262833484260eb9fcb86580436fd1917
oddlyspaced <oddlyspaced1@gmail.com>
2022-12-23 01:53:52 +0530

    rkcu: add methods to setup argparser

--- rkcu.py
+++ rkcu.py
@@ -1,4 +1,5 @@
 import argparse
+
 # Python based Command Line wrapper for managing color modes on Royal Kludge RK 61 Keyboard
 # author: Hardik Srivastava [oddlyspaced]
 
@@ -10,17 +11,23 @@ parser = argparse.ArgumentParser(
     description = 'Linux Utility to manage LED\'s on the Royal Kludge RK61 Keyboard.'
 )
 
-parser.add_argument('--speed', '-sp')
-parser.add_argument('--brightness', '-br')
-parser.add_argument('--sleep', '-sl')
-parser.add_argument('--animation', '-an')
+def setup_arg_parser():
+    global parser
+    parser.add_argument('--speed', '-sp')
+    parser.add_argument('--brightness', '-br')
+    parser.add_argument('--sleep', '-sl')
+    parser.add_argument('--animation', '-an')
 
-parser.add_argument('--rainbow', '-rb', action='store_true')
+    parser.add_argument('--rainbow', '-rb', action='store_true')
 
-parser.add_argument('--red', '-r')
-parser.add_argument('--green', '-g')
-parser.add_argument('--blue', '-b')
+    parser.add_argument('--red', '-r')
+    parser.add_argument('--green', '-g')
+    parser.add_argument('--blue', '-b')
 
-args = parser.parse_args()
+def read_args():
+    args = parser.parse_args()
+    var = vars(args)
+    print(var['speed'])
 
-print(args)
+setup_arg_parser()
+read_args()
