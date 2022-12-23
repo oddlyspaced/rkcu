38d4d1fc368f3264ec0a7972b0147be43d40bd05
oddlyspaced <oddlyspaced1@gmail.com>
2022-12-23 09:45:24 +0530

    test custom led config setting

---
+++ custom.py
@@ -0,0 +1,25 @@
+import hid
+
+vid = 0x258a
+pid = 0x004a
+
+rk61 = hid.enumerate(vid, pid)
+kb_path = rk61[1].get('path') # serial path for keyboard usb
+h = hid.device()
+h.open_path(kb_path)
+
+def gen_report(line: str):
+    global h
+    report = bytearray(65)
+    spl = line.split(" ")
+    for index in range(0, 65):
+        report[index] = int(spl[index], 16)
+    h.send_feature_report(bytes(report))
+
+f = open('custom_led_dump.txt', 'r')
+lines = f.readlines()
+f.close()
+
+for line in lines:
+    print(line)
+    gen_report(line)
